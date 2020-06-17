92cca08a0e8e39f27efb16e5ca04dd095943877e
sangji <parksangji1109@gmail.com>
2020-06-17 14:09:12 +0900

    Racket

--- Programming_Language/sol5.rkt
+++ Programming_Language/sol5.rkt
@@ -22,11 +22,13 @@
 
 ;; CHANGE (put your solutions here)
 
-(define (racketlist -> mupllist xs)
-  (if (null? xs) (aunit) (apair (car xs) (racketlist -> mupllist (cdr xs)))))
+(define (racketlist->mupllist l)
+   (cond [(null? l) (aunit)]
+ 	[(pair? l) (apair (car l) (racketlist->mupllist (cdr l)))]))
 
-(define (mupllist->racketlist list)
-  (if (aunit? list) null   (cons (apair-e1 list) (mupllist->racketlist (apair-e2 list)))))
+(define (mupllist->racketlist l)
+   (cond [(aunit? l) null]
+ 	[(apair? l) (cons (apair-e1 l) (mupllist->racketlist (apair-e2 l)))]))
 
 ;; Problem 2
 
@@ -154,3 +156,35 @@
 ;; Do NOT change this
 (define (eval-exp-c e)
   (eval-under-env-c (compute-free-vars e) null))
+
+
+(struct glet (var e body) #:transparent) ;; a global binding that overrides any local binding (let var = e in body)
+
+(struct num-array  (size) #:transparent)  ;; a number array  (initialized to zeroes), e.g., (num-array 10)
+                                                     ;; e.g. (num-array 4)
+
+(struct num-array-at   (e1 e2) #:transparent) ;; e1 evaluates to num-array and e2 evaluates to racket int (index of the value to access) index starts from 0
+                                              ;; (num-array-at (num-array 4) 3)
+                                              ;; (num-array-at (num-array 4) 4) ;  this should give a nice error message (like "array access out of bound")
+                                              ;; (num-array-at (num-array 4) -1) ;  this should give a nice error message (like "array access out of bound")
+
+(struct num-array-set  (e1 e2 e3) #:transparent) ;; e1 evaluates to num-array-var, e2 evaluates to racket int (index of the value to access), and e3 evaluates to a MUPL int
+                                              ;; (num-array-set (num-array 4) 0 (int 42))
+                                              ;; (num-array-set (num-array 4) 5 (int 42)) ; this should give a nice error message (like "array access out of bound")
+                                              ;; (num-array-set (num-array 4) -1 (int 42)) ; this should give a nice error message (like "array access out of bound")
+
+(define (num-array-object? v) ;; hackish implementation of testing num-array object. We assume that if a value is mpair, it is a num-array object.
+  (mpair? v))
+
+(define (array-length array)
+  (if (eq? (mcdr array) null)
+      1
+      (+ 1 (array-length (mcdr array)))))
+(define (make-array-object length)  
+    (if (= length 0)
+        null
+        (mcons (int 0) (make-array-object (- length 1)))))
+(define (set-array-val array index val)
+  (if (= index 0)
+      (set-mcar! array val)
+      (set-array-val (mcdr array) (- index 1) val)))
